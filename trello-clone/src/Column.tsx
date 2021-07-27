f2aeca73c1d5dc830feee4d9c8dd600b14739bde
Hoang Nguyen Khanh <n.khanhhoang@gmail.com>
2021-07-27 07:30:44 +0700

    Initialize Column, Card

---
+++ trello-clone/src/Column.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ColumnContainer, ColumnTitle } from './styles'
+
+interface IColumnProps {
+  text?: string
+}
+
+export const Column = ({
+  text,
+  children,
+}: React.PropsWithChildren<IColumnProps>) => {
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{text}</ColumnTitle>
+      {children}
+    </ColumnContainer>
+  )
+}
