bb081142bf218955617b46ee7f67e2f9763eadc5
Hoang Nguyen Khanh <n.khanhhoang@gmail.com>
2021-08-08 17:33:48 +0700

    [TrelloClone] Get data from app state

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -10,8 +10,12 @@ const App = () => {
 
   return (
     <AppContainer>
-      {state.lists.map((list, index) => (
-        <Column text={list.text} key={list.id} />
+      {state.lists.map((list) => (
+        <Column text={list.text} key={list.id}>
+          {list.tasks.map((task) => (
+            <Card text={task.text} key={task.id}></Card>
+          ))}
+        </Column>
       ))}
       <AddNewItem
         toggleButtonText="+ Add another list"
